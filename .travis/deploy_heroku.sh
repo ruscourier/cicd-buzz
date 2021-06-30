affdd7989549d278ec67f199b185a79ae3925c0f
Andrey <ruscourier@gmail.com>
2021-06-30 15:49:21 +0300

    9th commit

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 curl https://cli-assets.heroku.com/install.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
-docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:login
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME
