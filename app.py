cde10cd78db14f993c029240d4544964e7a6ac62
Andrey <ruscourier@gmail.com>
2021-06-30 16:13:45 +0300

    12th commit

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
-import signal
 from flask import Flask
 from buzz import generator
 
 app = Flask(__name__)
 
-signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
@@ -13,6 +11,7 @@ def generate_buzz():
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
